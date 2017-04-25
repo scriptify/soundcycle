3ee9adc0eaf362f29a2a5461f27c27e781c1a6ac
scriptify <maximilian.torggler@tuta.io>
2017-04-25 17:48:46 +0200

    Logo now placed in bg.

--- app/components/frequency-canvas/index.jsx
+++ app/components/frequency-canvas/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import './style.css';
+import logo from 'icons/logo.png';
 
 let canvas;
+const logoImg = new Image();
+logoImg.src = logo;
 
 const FrequencyCanvas = ({ data }) => {
 
@@ -15,24 +18,32 @@ const FrequencyCanvas = ({ data }) => {
     canvasCtx.fillStyle = 'rgba(0, 0, 0, 0)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    var barWidth = (WIDTH / data.length) * 2.5;
-    var barHeight;
-    var x = 0;
+    const barWidth = (WIDTH / data.length) * 2.5;
+    let barHeight;
+    let x = 0;
+    let avgVal = 0;
 
-    for(var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       barHeight = data[i] / 2;
+      avgVal += data[i];
 
       canvasCtx.fillStyle = `rgb(80, 80, ${ 80 + barHeight })`;
       canvasCtx.fillRect(x, HEIGHT/2 - barHeight/2, barWidth, barHeight);
 
       x += barWidth + 1;
     }
+
+    avgVal = avgVal / data.length;
+    const IMG_H = 100 + avgVal;
+    const IMG_W = 100 + avgVal;
+
+    canvasCtx.drawImage(logoImg, WIDTH/2 - IMG_W/2, HEIGHT/2 - IMG_H/2, IMG_W, IMG_H);
   }
 
   return (
     <div className={`frequency`}>
       <canvas width={WIDTH} height={HEIGHT} ref={(r) => {
-        if(r)
+        if (r)
           canvas = r;
       }}></canvas>
     </div>
