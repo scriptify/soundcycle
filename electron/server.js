9c37be11bb3bc71776f708aac536edefd33d903d
scriptify <scriptify@localhost.localdomain>
2017-06-04 18:10:34 +0200

    Fixed wrong rehydration problem.

--- electron/server.js
+++ electron/server.js
@@ -5,15 +5,22 @@ const io = require('socket.io')(http);
 function createServer({ port }) {
   // Server should save actions: When a new client connects, all
   // actions can be executed on it, so it's in sync with the rest
-  const actions = [];
+  let actions = [];
+  let isLoopstation = true;
 
   app.get('/', (req, res) => {
     res.send(`<p>Controller server started! Open the loopstation on your controlling device by appending /#controller</p>`);
   });
 
   io.on('connection', (socket) => {
-    // Initialize with action
-    actions.forEach(a => socket.emit(`action`, a));
+
+    if (isLoopstation) {
+      isLoopstation = false;
+      socket.on(`disconnect`, () => actions = []);
+    } else {
+      // Initialize with action
+      actions.forEach(a => socket.emit(`action`, a));
+    }
 
     socket.on(`action`, (data) => {
       actions.push(data);
